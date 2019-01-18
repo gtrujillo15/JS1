b7fd043197b80455394a1afc4c29904f9ed99431
Gabriela <gtrujillo15@gmail.com>
2019-01-17 18:31:11 -0800

    practiced console log and arrays

---
+++ JS/script.js
@@ -0,0 +1,45 @@
+// console.log("hi, i'm javascript");
+var name = "Gabriela";
+// var is variable
+// name is the name of the variable
+// = mean assigning value to 'name' variable
+// Gabriela is the value assigned to the 'name' variable
+// TYPE string
+var name = "Gabriela";
+// TYPE number
+var age = 5;
+// TYPE array
+var arr = [5, 2, 52, "abc", "grt"];
+// TYPE object
+var obj = {
+  // the firstame and lastname is property
+  firstName: 'Gabriela',
+  lastName: 'Trujillo',
+  age: 10
+};
+
+// console.log("hi, i'm " + name);
+// you're concantoning the two things together
+
+var greeting = "hi, i'm " + name;
+console.log(greeting);
+
+var anotherGreeting = "hi, i'm"
+
+// display your name and age in different ways
+
+console.log("my last name is", obj.lastName);
+console.log("my name is", obj['firstName']);
+
+var madeup = 'value';
+var i = 0;
+i=1;
+
+const me = {
+  name: 'Gabriela',
+  hair: 'Brown',
+  age: 26,
+  ethnicity: latina,
+}
+
+console.log(me.name + me.hair + me.age + me.ethnicity);
